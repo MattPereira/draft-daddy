919724b0ae14c735cdfc2bc86c7c6ab755500c79
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-05-29 13:55:47 -0700

    convert array into hashmap for faster lookup speed

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -19,12 +19,16 @@ router.get("/", async function (req, res, next) {
     key: process.env.API_KEY,
   });
 
+  // slicing first row because brad made some useless headers in google sheets
   const rawData = sheetsApiResponse.data.values.slice(1);
 
+  // the keys are the first item in raw data aray
   const keys = rawData[0];
+  // the values are everything after the first item in raw data array
   const values = rawData.slice(1);
 
-  const prettyData = values.map((row) => {
+  // map over values array and create an object for each row instead of having a bunch of nested arrays
+  const objectArray = values.map((row) => {
     let obj = {};
 
     row.forEach((value, idx) => {
@@ -34,7 +38,18 @@ router.get("/", async function (req, res, next) {
     return obj;
   });
 
-  return res.json({ data: prettyData });
+  const initialValue = {};
+
+  // reduce the array of objects into a single object with child objects that have key of id
+  const objectsOnly = objectArray.reduce((obj, item) => {
+    return {
+      ...obj,
+      [item["id"]]: item,
+    };
+  }, initialValue);
+
+  // returning hashmap instead of array for faster lookup speeds
+  return res.json({ data: objectsOnly });
 });
 
 module.exports = router;
