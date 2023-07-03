34ec5c8a7e260fce8cfce3cc4adba67929164c0d
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-07-03 13:05:49 -0700

    add serverless function for google sheets api request

---
+++ frontend/app/api/overlay/route.js
@@ -0,0 +1,43 @@
+const { google } = require("googleapis");
+
+const sheets = google.sheets("v4");
+
+export async function GET(request) {
+  console.log(process.env.GOOGLE_SHEETS_API_KEY);
+
+  const sheetsResponse = await sheets.spreadsheets.values.get({
+    spreadsheetId: "1qXX_OziGRULreow3w2YhvvJR__lYSyothvptO1MdZ3c",
+    range: "Combined Exposure!A3:Z500",
+    key: process.env.GOOGLE_SHEETS_API_KEY,
+  });
+
+  const rawData = sheetsResponse.data.values;
+
+  // the keys are the first item in raw data aray
+  const keys = rawData[0];
+  // the values are everything after the first item in raw data array
+  const values = rawData.slice(1);
+
+  // map over values array and create an object for each row instead of having a bunch of nested arrays
+  const objectArray = values.map((row) => {
+    let obj = {};
+
+    row.forEach((value, idx) => {
+      obj[keys[idx]] = value;
+    });
+
+    return obj;
+  });
+
+  const initialValue = {};
+
+  // reduce the array of objects into a single object with child objects that have key of id
+  const data = objectArray.reduce((obj, item) => {
+    return {
+      ...obj,
+      [item["id"]]: item,
+    };
+  }, initialValue);
+
+  return Response.json({ data: data });
+}
--- start.sh
+++ start.sh
@@ -3,10 +3,31 @@
 # Save the current working directory
 PROJECT_DIR=$(pwd)
 
-# start the nextjs frontend
+# Start the nextjs frontend
 cd "$PROJECT_DIR/frontend" && npm run dev &
+# Save the PID of the frontend process
+FRONTEND_PID=$!
 
-# start the django backend
-cd "$PROJECT_DIR/backend" && source venv/bin/activate && python manage.py runserver
+# Go back to the original working directory
+cd "$PROJECT_DIR"
+
+# Start the django backend
+cd "$PROJECT_DIR/backend" && source venv/bin/activate && python manage.py runserver &
+# Save the PID of the backend process
+BACKEND_PID=$!
+
+# Define cleanup procedure
+cleanup() {
+    echo "Stopping servers..."
+    kill $FRONTEND_PID
+    kill $BACKEND_PID
+}
+
+# Set the cleanup procedure to run when the script receives an interrupt signal (e.g., when you press Ctrl+C)
+trap cleanup INT
+
+# Wait for either server to exit; this should keep your script running until you press Ctrl+C
+wait $FRONTEND_PID $BACKEND_PID
+
 
 
